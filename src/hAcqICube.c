e096072c8e183e7d53f55b31ad23d92fa9e9db2c
Aaron Mavrinac <mavrinac@gmail.com>
2011-10-28 16:03:46 -0400

    Attempt to support multiple camera instances.

--- src/hAcqICube.c
+++ src/hAcqICube.c
@@ -23,20 +23,21 @@
 extern "C" {
 #endif
     extern HUserExport Herror FGInit(Hproc_handle proc_id, FGClass *fg);
-    extern HLibExport  Herror IOPrintErrorMessage(char *err);
+    extern HLibExport Herror IOPrintErrorMessage(char *err);
 #ifdef __cplusplus
 }
 #endif
 
 typedef struct
 {
-    /* TODO: TFGInstance stuff */
+    INT index;
 } TFGInstance;
 
 static FGClass * fgClass;
 
 static TFGInstance FGInst[FG_MAX_INST];
-static INT numInstance = 0;
+static INT num_instances = 0;
+static INT num_devices = 0;
 
 static char errMsg[MAX_STRING];
 
@@ -44,7 +45,12 @@ static Herror FGOpen(Hproc_handle proc_id, FGInstance * fginst)
 {
     TFGInstance * currInst = (TFGInstance *)fginst->gen_pointer;
 
-    /* TODO: FGOpen */
+    if(!NETUSBCAM_Open(currInst->index))
+    {
+        MY_PRINT_ERROR_MESSAGE("open camera failed")
+        return H_ERR_FGNI;
+    }
+    num_instances++;
 
     return H_MSG_OK;
 }
@@ -53,7 +59,12 @@ static Herror FGClose (Hproc_handle proc_id, FGInstance * fginst)
 {
     TFGInstance * currInst = (TFGInstance *)fginst->gen_pointer;
 
-    /* TODO: FGClose */
+    if(!NETUSBCAM_Close(currInst->index))
+    {
+        MY_PRINT_ERROR_MESSAGE("close camera failed")
+        return H_ERR_FGCLOSE;
+    }
+    num_instances--;
 
     return H_MSG_OK;
 }
@@ -67,7 +78,19 @@ static Herror FGGrab(Hproc_handle proc_id, FGInstance * fginst, Himage * image, 
 {
     TFGInstance * currInst = (TFGInstance *)fginst->gen_pointer;
 
-    /* TODO: FGGrab */
+    if(!NETUSBCAM_Start(currInst->index))
+    {
+        MY_PRINT_ERROR_MESSAGE("start camera failed")
+        return H_ERR_FGF;
+    }
+
+    /* TODO: what now? */
+
+    if(!NETUSBCAM_Stop(currInst->index))
+    {
+        MY_PRINT_ERROR_MESSAGE("stop camera failed")
+        return H_ERR_FGF;
+    }
 
     return H_MSG_OK;
 }
@@ -80,7 +103,7 @@ static Herror FGGrabAsync(Hproc_handle proc_id, FGInstance * fginst, double maxD
 static Herror FGInfo(Hproc_handle proc_id, INT queryType, char ** info, Hcpar ** values, INT * numValues)
 {
     Hcpar *val;
-    INT    i;
+    INT i;
 
     switch(queryType)
     {
@@ -95,24 +118,24 @@ static Herror FGInfo(Hproc_handle proc_id, INT queryType, char ** info, Hcpar **
             break;
         case FG_QUERY_PARAMETERS:
             *info = "Additional parameters for this image acquisition interface.";
-            HCkP(HAlloc(proc_id, (size_t)(FG_PARAM_NUM * sizeof(Hcpar)), &val));
             /* TODO: specify all parameter names and types */
-            *values = val;
-            *numValues = FG_PARAM_NUM;
+            /*HCkP(HAlloc(proc_id, (size_t)(0 * sizeof(Hcpar)), &val));*/
+            *values = NULL;
+            *numValues = 0;
             break;
         case FG_QUERY_PARAMETERS_RO:
             *info = "Additional read-only parameters for this interface.";
-            HCkP(HAlloc(proc_id, (size_t)(FG_PARAM_RO_NUM * sizeof(Hcpar)), &val));
             /* TODO: specify all parameter names and types */
-            *values = val;
-            *numValues = FG_PARAM_RO_NUM;
+            /*HCkP(HAlloc(proc_id, (size_t)(0 * sizeof(Hcpar)), &val));*/
+            *values = NULL;
+            *numValues = 0;
             break;
         case FG_QUERY_PARAMETERS_WO:
             *info = "Additional write-only parameters for this interface.";
-            HCkP(HAlloc(proc_id, (size_t)(FG_PARAM_WO_NUM * sizeof(Hcpar)), &val));
             /* TODO: specify all parameter names and types */
-            *values = val;
-            *numValues = FG_PARAM_WO_NUM;
+            /*HCkP(HAlloc(proc_id, (size_t)(0 * sizeof(Hcpar)), &val));*/
+            *values = NULL;
+            *numValues = 0;
             break;
         case FG_QUERY_REVISION:
             *info = "Current interface revision.";
@@ -127,6 +150,17 @@ static Herror FGInfo(Hproc_handle proc_id, INT queryType, char ** info, Hcpar **
             *values = NULL;
             *numValues = 0;
             break;
+        case FG_QUERY_PORT:
+            *info = "List of available camera device indices.";
+            HCkP(HAlloc(proc_id, (size_t)(num_devices * sizeof(Hcpar)), &val));
+            for(i = 0; i < num_devices; i++)
+            {
+                val[i].par.l = i;
+                val[i].type = LONG_PAR;
+            }
+            *values = val;
+            *numValues = num_devices;
+            break;
         case FG_QUERY_INFO_BOARDS:
         case FG_QUERY_BITS_PER_CHANNEL:
         case FG_QUERY_COLOR_SPACE:
@@ -134,7 +168,6 @@ static Herror FGInfo(Hproc_handle proc_id, INT queryType, char ** info, Hcpar **
         case FG_QUERY_EXT_TRIGGER:
         case FG_QUERY_FIELD:
         case FG_QUERY_GENERIC:
-        case FG_QUERY_PORT:
         case FG_QUERY_HORIZONTAL_RESOLUTION:
         case FG_QUERY_VERTICAL_RESOLUTION:
             *info = "Unused.";
@@ -203,6 +236,13 @@ Herror FGInit(Hproc_handle proc_id, FGClass * fg)
     fgClass = fg;
 
     /* TODO: set default parameter values */
+
+    num_devices = NETUSBCAM_Init();
+    if(num_devices <= 0)
+    {
+        MY_PRINT_ERROR_MESSAGE("no camera detected")
+        return H_ERR_FGNI;
+    }
 
     return H_MSG_OK;
 }
